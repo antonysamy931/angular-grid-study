1e722a3744701bc13b172b7b83eeb78b50b1f1c9
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2017-09-20 20:12:10 +0530

    First commit

---
+++ AngularGrid/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularGrid.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AngularGrid/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularGrid.Models;
+
+namespace AngularGrid.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        AEPEntities _entities = null;
+        public ValuesController()
+        {
+            _entities = new AEPEntities();
+        }
+
+        // GET api/values
+        public object Get()
+        {
+            return _entities.Lessons.ToList();
+        }
+
+        // GET api/values/5
+        public object Get(int id)
+        {
+            return _entities.Lessons.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        public void Post(Lesson model)
+        {
+            _entities.Lessons.Add(model);
+            _entities.SaveChanges();
+        }
+
+        // PUT api/values/5
+        public void Put(int id, Lesson model)
+        {
+            Lesson ExistingLession = _entities.Lessons.Where(x => x.Id == id).FirstOrDefault();
+            ExistingLession.Description = model.Description;
+            ExistingLession.Name = model.Name;
+            _entities.SaveChanges();
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Lesson LessionObject = _entities.Lessons.Where(x => x.Id == id).FirstOrDefault();
+            _entities.Lessons.Remove(LessionObject);
+            _entities.SaveChanges();
+        }
+    }
+}
